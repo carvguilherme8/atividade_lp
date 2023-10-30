59a75544cea597f12cc3c31155134003c352e598
Gustavo Tironi <b51083@fgv.edu.br>
2023-10-30 18:45:41 -0300

    Teste do inventario na main

--- inventario.py
+++ inventario.py
@@ -22,29 +22,14 @@ class Inventario():
             n = 0
             while n < qtd_prod:
                 self.estoque[prod.tipo].pop()
-                self.caixa += prod.valor*qtd_prod 
+                self.caixa += prod.valor
                 n += 1
         else:
             raise ErroVenda()
 
     def retorno_produto(self, prod, qtd_prod):
         if self.caixa >= prod.valor*qtd_prod:
-            self.caixa -= prod.valor*qtd_prod
+            self.caixa -= prod.valor
             self.estoque[prod.tipo].append(prod.__str__())
         else:
             raise ErroRetorno
-
-
-
-
-        if prod in self.estoque:
-            self.estoque[prod] += qtd_prod
-        else:
-            self.estoque[prod] = qtd_prod
-
-        if self.caixa >= valor*qtd_prod:
-            self.caixa -= valor*qtd_prod
-        else:
-            raise ErroRetorno()
-    #subtrai qtd do dicionario
-    #retorna dinheiro ao cliente
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import produtos as prod
+import inventario as inv
 
+inventario = inv.Inventario()
 
 nitro_5 = prod.Laptop("Nitro 5", 4199, "1111111", prod.Marcas(2).name, "15,6”", "8GB", "4GB", "4,5 GHz")
+
+
+
+inventario.reposicao(nitro_5, 5)
+
+inventario.venda_produto(nitro_5, 3)
+
+inventario.retorno_produto(nitro_5, 1)
+
+print(inventario.caixa)
+print(inventario.estoque)
