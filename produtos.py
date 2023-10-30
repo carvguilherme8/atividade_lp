71bf120961495dbdaace55c8e2c31ebe0fdf7c65
LUÍS FELIPE DE ABREU MARCIANO <B51064@fgv.edu.br>
2023-10-30 16:20:28 -0300

    criei os produtos

--- produtos.py
+++ produtos.py
@@ -6,13 +6,28 @@ class Produto():
         self.cod_barras = cod_barras        
 
 
-class Filme(Produto):
+class Celular(Produto):
 
-    def __init__(self, name, valor, cod_barras, genero, duracao)
-        super().init(self, name, valor, cod_barras)
-        self.genero = genero
-        self.duracao = duracao
+    def __init__(self, name, valor, cod_barras, marca, tamanho, memoria_ram):
+        super().__init__(self, name, valor, cod_barras)
+        self.marca = marca
+        self.tamanho = tamanho
+        self.mamoria_ram = memoria_ram
 
+class Laptop(Produto):
+    
+    def __init__(self, name, valor, cod_barras, marca, tamanho, memoria_ram, memoria_vram, freq_processador):
+        super().__init__(name, valor, cod_barras)
+        self.marca = marca
+        self.tamanho = tamanho
+        self.memoria_ram = memoria_ram
+        self.memoria_vram = memoria_vram
+        self.freq_processador = freq_processador
 
-class Livro()
-
+class Tablet(Produto):
+    def __init__(self, name, valor, cod_barras, marca, tamanho, memoria_ram, possui_caneta):
+        super().__init__(self, name, valor, cod_barras)
+        self.marca = marca
+        self.tamanho = tamanho
+        self.memoria_ram = memoria_ram
+        self.possui_caneta = possui_caneta
