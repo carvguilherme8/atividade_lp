0d856edf4b6690e046aff7d512f95e1cf5d68f86
LUÍS FELIPE DE ABREU MARCIANO <B51064@fgv.edu.br>
2023-10-30 18:47:36 -0300

    Criei um tablet

--- main.py
+++ main.py
@@ -3,10 +3,5 @@ import produtos as prod
 
 nitro_5 = prod.Laptop("Nitro 5", 4199, "1111111", prod.Marcas(2).name, "15,6”", "8GB", "4GB", "4,5 GHz")
 
-print(nitro_5)
+tab_s6 = prod.Tablet("Galaxy Tab S6 Lite", 1729, "2222222", prod.Marcas(2).name, "10.4", "4GB", True)
 
-lista = []
-
-lista.append(nitro_5.__str__())
-
-print(lista)
