a5008f7afd51f2c0e1f0568173d1a66c8c42e2ce
GUILHERME CARVALHO CUNHA <B51072@fgv.edu.br>
2023-10-30 17:14:41 -0300

    adicoes inventario

--- inventario.py
+++ inventario.py
@@ -1,35 +1,53 @@
+#Add classes de tratamento de erros nas vendas e retornos
+
 class ErroVenda(Exception):
-    def __init__(self, message):
+    def __init__(self, message="Quantidade não disponível no estoque"):
         self.message = message
         super().__init__(self.message)
 
+class ErroRetorno(Exception):
+    def __init__(self, message="Sem dinheiro para retornar ao cliente"):
+        self.message = message
+        super().__init__(self.message)
+
+
 class Inventario:
+    
     def __init__(self):
         self.estoque = {}
+        self.caixa = 0
 
     def reposicao(self, prod, qtd_prod):
         if prod in self.estoque:
             self.estoque[prod] += qtd_prod
         else:
             self.estoque[prod] = qtd_prod
+    #cria a chave no dicionario e adiciona o qtd ou apenas soma a qtd caso chave (produto) exista
 
-    def venda_prod(self, prod, qtd_prod):
+    def venda_prod(self, prod, qtd_prod, valor):
         if prod in self.estoque and self.estoque[prod] >= qtd_prod:
             self.estoque[prod] -= qtd_prod
+            self.caixa += valor*qtd_prod
         else:
-            raise ErroVenda("Quantidade não disponível no estoque")
+            raise ErroVenda()
+    #subtrai valor (qtd) da chave (produto) do dicionario
+    #add dinheiro no caixa
 
-    def retorno_prod(self, prod, qtd_prod):
+    def retorno_prod(self, prod, qtd_prod, valor):
         if prod in self.estoque:
             self.estoque[prod] += qtd_prod
         else:
             self.estoque[prod] = qtd_prod
 
-# Cria uma instância da classe Inventario
+        if self.caixa >= valor*qtd_prod:
+            self.caixa -= valor*qtd_prod
+        else:
+            raise ErroRetorno()
+    #subtrai qtd do dicionario
+    #retorna dinheiro ao cliente
+        
+
+# Criei uma instância do Inventario
 inventario = Inventario()
 
-inventario.venda_prod("camisa",2)
-inventario.venda_prod("camisa",3)
-
-print(inventario.estoque)
 
