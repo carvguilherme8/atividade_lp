87e18e422d052731cae4a5f486c1c1193ab1d066
GUILHERME CARVALHO CUNHA <B51072@fgv.edu.br>
2023-10-30 14:49:22 -0300

    base classes

---
+++ produtos.py
@@ -0,0 +1,18 @@
+class Produto():
+
+    def __init__(self, nome, valor, cod_barras):
+        self.nome = nome
+        self.valor = valor
+        self.cod_barras = cod_barras        
+
+
+class Filme(Produto):
+
+    def __init__(self, name, valor, cod_barras, genero, duracao)
+        super().init(self, name, valor, cod_barras)
+        self.genero = genero
+        self.duracao = duracao
+
+
+class Livro()
+
